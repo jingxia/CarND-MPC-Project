d1441b17f11901dac8364a6983fae0905434ddc5
jiajunx <jiajunxu20@gmail.com>
2018-05-10 21:55:03 -0400

    small change

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -54,24 +54,24 @@ class FG_eval {
     // The part of the cost based on the reference state.
     for (int t = 0; t < N; t++) 
     {
-      //fg[0] += 8 * CppAD::pow(vars[cte_start + t], 2);
-      //fg[0] += 2 * CppAD::pow(vars[epsi_start + t], 2);
-      fg[0] += 100 * CppAD::pow(vars[v_start + t] - ref_v, 2);
+      fg[0] += CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
     // Minimize the use of actuators.
-    //for (int t = 0; t < N - 1; t++) 
-    //{
-    //  fg[0] += 600000 * CppAD::pow(vars[delta_start + t], 2);
-    //  fg[0] += 300 * CppAD::pow(vars[a_start + t], 2);
-    //}
+    for (int t = 0; t < N - 1; t++) 
+    {
+      fg[0] += CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += CppAD::pow(vars[a_start + t], 2);
+    }
 
     //// Minimize the value gap between sequential actuations.
-    //for (int t = 0; t < N - 2; t++) 
-    //{
-    //  fg[0] += 300 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
-    //  fg[0] += 300 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
-    //}
+    for (int t = 0; t < N - 2; t++) 
+    {
+      fg[0] += 300 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 300 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
+    }
    
     fg[1 + x_start] = vars[x_start];
     fg[1 + y_start] = vars[y_start];
@@ -241,9 +241,6 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
 
   // Check some of the solution values
   ok &= solution.status == CppAD::ipopt::solve_result<Dvector>::success;
-
-  // Cost
-  auto cost = solution.obj_value;
 
   // TODO: Return the first actuator values. The variables can be accessed with
   // `solution.x[i]`.
